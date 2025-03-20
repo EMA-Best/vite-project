d2439c3a0c87e0a9f559c229a0d45b9622cae201
EMA-Best <1969537654@qq.com>
2025-03-20 21:06:47 +0800

    未格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+function a (){
+  console.log("1");
+  
+}
+a();
+function b () {
+  console.log("2");
+  
+}
+b();
+</script>
+
+<template>
+  <h2>APP</h2>
+</template>
+
+<style scoped>
+.main {
+  width: 100px;
+}
+</style>
